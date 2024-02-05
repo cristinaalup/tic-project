b6dadc1fdbf98a1bf7acc3ef305e1b2ff79d590b
cristinaalup <cristinalupascu418@gmail.com>
2024-02-05 12:57:51 +0200

    responsive carusel

--- client/src/components/ProjectsCarousel.vue
+++ client/src/components/ProjectsCarousel.vue
@@ -1,17 +1,14 @@
 <template>
-  <v-carousel hide-delimiters v-if="chunkedProjects.length > 0" >
+  <v-carousel hide-delimiters v-if="chunkedProjects.length > 0">
     <v-carousel-item v-for="(chunk, index) in chunkedProjects" :key="index">
       <v-row class="carousel-row">
         <v-col
           v-for="project in chunk"
           :key="project.id"
-          cols="12"
-          sm="6"
-          md="4"
-          lg="4"
-        > 
+          :cols="getResponsiveColumns()"
+        >
           <template v-if="project && project.id !== undefined">
-            <ProjectCard :project="project"/>
+            <ProjectCard :project="project" />
           </template>
         </v-col>
       </v-row>
@@ -30,7 +27,7 @@ export default {
       return this.$store.state.projects;
     },
     chunkedProjects() {
-      const chunkSize = 3;
+      const chunkSize = this.calculateChunkSize();
       const resultArray = [];
       for (let i = 0; i < this.projects?.length; i += chunkSize) {
         resultArray.push(this.projects.slice(i, i + chunkSize));
@@ -41,19 +38,46 @@ export default {
   async created() {
     await this.$store.dispatch("fetchProjects");
   },
+  methods: {
+    calculateChunkSize() {
+      const screenWidth = window.innerWidth;
+      if (screenWidth < 790) {
+        return 1;
+      } else {
+        return 3;
+      }
+    },
+    getResponsiveColumns() {
+      return {
+        xs: 12,
+        sm: 6,
+        md: 4,
+        lg: 3,
+      };
+    },
+  },
 };
 </script>
 <style scoped>
 .carousel-row {
   justify-content: space-around;
-  margin: 10px 60px;
+  margin: 10px 10px;
 }
 .v-carousel {
-  max-width: 1500px;
+  max-width: 100%;
   margin: 0 auto;
 }
 .v-carousel .v-carousel__controls .v-icon {
-  backdrop-filter: blur(5px); /* Efect de blur */
-  opacity: 0.7; /* Opacitatea pentru a face săgețile transparente */
+  backdrop-filter: blur(5px);
+  opacity: 0.7;
+}
+@media only screen and (max-width: 600px) {
+  .login-container {
+    padding: 15px;
+  }
+  .container-styling {
+    width: 100%;
+    height: 400px;
+  }
 }
 </style>
