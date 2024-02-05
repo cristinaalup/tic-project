e54ad68ab254ec2d8f127274052ae560d729127a
cristinaalup <cristinalupascu418@gmail.com>
2024-02-05 12:49:30 +0200

    remove search

--- client/src/components/ProjectsCarousel.vue
+++ client/src/components/ProjectsCarousel.vue
@@ -1,59 +1,27 @@
 <template>
-  <div>
-    <div>
-      <v-row
-        justify="center"
-        class="mx-auto container-styling"
-        :class="{ 'flex-column': isMobileOrTablet }"
-      >
-        <v-text-field
-          v-model="ownerFilter"
-          label="Search by owner"
-          prepend-inner-icon="mdi-filter"
-        />
-
-        <v-text-field
-          v-model="projectNameFilter"
-          label="Search by project name"
-          prepend-inner-icon="mdi-filter"
-        />
+  <v-carousel hide-delimiters v-if="chunkedProjects.length > 0" >
+    <v-carousel-item v-for="(chunk, index) in chunkedProjects" :key="index">
+      <v-row class="carousel-row">
+        <v-col
+          v-for="project in chunk"
+          :key="project.id"
+          cols="12"
+          sm="6"
+          md="4"
+          lg="4"
+        > 
+          <template v-if="project && project.id !== undefined">
+            <ProjectCard :project="project"/>
+          </template>
+        </v-col>
       </v-row>
-    </div>
-    <v-carousel
-      hide-delimiters
-      v-if="getFilteredAndChunkedProjects().length > 0"
-    >
-      <v-carousel-item
-        v-for="(chunk, index) in getFilteredAndChunkedProjects()"
-        :key="index"
-      >
-        <v-row class="carousel-row">
-          <v-col
-            v-for="project in chunk"
-            :key="project.id"
-            :cols="getResponsiveColumns()"
-          >
-            <template v-if="project && project.id !== undefined">
-              <ProjectCard :project="project" />
-            </template>
-          </v-col>
-        </v-row>
-      </v-carousel-item>
-    </v-carousel>
-  </div>
+    </v-carousel-item>
+  </v-carousel>
 </template>
 
 <script>
 import ProjectCard from "./ProjectCard.vue";
-
 export default {
-  data() {
-    return {
-      ownerFilter: "",
-      projectNameFilter: "",
-      isMobileOrTablet: window.innerWidth <= 600,
-    };
-  },
   components: {
     ProjectCard,
   },
@@ -61,102 +29,31 @@ export default {
     projects() {
       return this.$store.state.projects;
     },
-  },
-  mounted() {
-    window.addEventListener("resize", this.handleWindowResize);
-  },
-  beforeUnmount() {
-    window.removeEventListener("resize", this.handleWindowResize);
-  },
-  async created() {
-    await this.$store.dispatch("fetchUser");
-    await this.$store.dispatch("fetchProjects");
-  },
-  methods: {
-    getFilteredAndChunkedProjects() {
-      const filteredProjects = this.projects.filter((project) => {
-        const ownerMatch = project.owner.displayName
-          .toLowerCase()
-          .includes(this.ownerFilter.toLowerCase());
-        const projectNameMatch = project.name
-          .toLowerCase()
-          .includes(this.projectNameFilter.toLowerCase());
-
-        return ownerMatch && projectNameMatch;
-      });
-
-      const chunkSize = this.calculateChunkSize();
+    chunkedProjects() {
+      const chunkSize = 3;
       const resultArray = [];
-
-      for (let i = 0; i < filteredProjects.length; i += chunkSize) {
-        resultArray.push(filteredProjects.slice(i, i + chunkSize));
+      for (let i = 0; i < this.projects?.length; i += chunkSize) {
+        resultArray.push(this.projects.slice(i, i + chunkSize));
       }
-
       return resultArray;
     },
-    calculateChunkSize() {
-      const screenWidth = window.innerWidth;
-
-      if (screenWidth < 790) {
-        return 1;
-      } else {
-        return 3;
-      }
-    },
-    getResponsiveColumns() {
-      return {
-        xs: 12,
-        sm: 6,
-        md: 4,
-        lg: 3,
-      };
-    },
-    handleWindowResize() {
-      this.isMobileOrTablet = window.innerWidth <= 600;
-      this.$forceUpdate();
-    },
+  },
+  async created() {
+    await this.$store.dispatch("fetchProjects");
   },
 };
 </script>
-
 <style scoped>
-.container-styling {
-  width: 90%;
-  margin: 20px;
-  padding: 15px;
-  background: #7986cb;
-  backdrop-filter: blur(5px);
-  border-radius: 10px;
-  border-color: #7986cb;
-  color: aliceblue;
-  gap: 10px;
-}
-
 .carousel-row {
   justify-content: space-around;
-  margin: 10px 10px;
+  margin: 10px 60px;
 }
-
 .v-carousel {
-  max-width: 100%;
+  max-width: 1500px;
   margin: 0 auto;
 }
-
 .v-carousel .v-carousel__controls .v-icon {
-  backdrop-filter: blur(5px);
-  opacity: 0.7;
-}
-
-@media only screen and (max-width: 600px) {
-  .login-container {
-    padding: 15px;
-  }
-  .container-styling {
-    width: 90%;
-    height: 400px;
-  }
-  .flex-column {
-    flex-direction: column;
-  }
+  backdrop-filter: blur(5px); /* Efect de blur */
+  opacity: 0.7; /* Opacitatea pentru a face săgețile transparente */
 }
 </style>
